2a10b6b4d7ce43e72f31034ac721d087a810e1f7
KhaledAbdellaty <kabdellaty@gmail.com>
2024-07-17 03:50:48 +0300

    14. Top students

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''def top_students(mongo_collection)'''
+
+
+def top_students(mongo_collection):
+    '''A function that returns all students
+    sorted by average score.
+    '''
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
